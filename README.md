c5a2a34d8467b610a0e1077a68a9ab3574b24400
Val <valmack@protonmail.com>
2020-01-17 15:12:21 -0500

    Custom package layout

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-suis
+# suis
+
+Spatial Universal Interaction System for VR
+
+## About
+
+Suis removes the headaches of building virtual experiences for any device.
---
+++ Runtime/InputManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class InputManager : MonoBehavior
+{
+  // Start is called before the first frame update
+  void Start() {}
+  // Update is called once per frame
+  void Update() {
+    //
+  }
+}
