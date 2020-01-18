abf19ceb3eae16631764c4ede94ceaed01f98ddb
technobaboo <technobaboo@gmail.com>
2020-01-17 17:12:37 -0700

    Added many SDFs

---
+++ Assets/SUIS/Scripts/SDFs/DistanceTo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SUIS;
+
+public class DistanceTo : MonoBehaviour
+{
+    [SerializeField]
+    float distance = 0.0f;
+
+    public SDF sdf;
+
+    [ExecuteInEditMode]
+    void LateUpdate() {
+        distance = sdf.distance(transform.position);
+    }
+}
---
+++ Assets/SUIS/Scripts/SDFs/SDF.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SUIS {
+
+    public abstract class SDF : MonoBehaviour {
+        public abstract float distance(Vector3 point);
+
+        protected static Vector3 transformPoint(Vector3 point, Transform origin) {
+            Quaternion inverseRotation = Quaternion.Inverse(origin.rotation);
+            Vector3 inverseScale = Vector3.zero;
+            inverseScale.x = 1 / origin.lossyScale.x;
+            inverseScale.y = 1 / origin.lossyScale.y;
+            inverseScale.z = 1 / origin.lossyScale.z;
+
+            point -= origin.position;
+            point = inverseRotation * point;
+            point = Vector3.Scale(point, inverseScale);
+            return point;
+        }
+    }
+
+}
