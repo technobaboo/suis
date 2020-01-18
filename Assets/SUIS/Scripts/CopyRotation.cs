7deac78736c20a3fd93e354ddd89205928cd477b
technobaboo <technobaboo@gmail.com>
2020-01-18 18:19:53 -0500

    Added spin object based on head movement

---
+++ Assets/SUIS/Scripts/CopyRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyRotation : MonoBehaviour {
+    public Transform target;
+    public Rigidbody rigidbody;
+    public float oldAngle = 0;
+    void Update() {
+        rigidbody.AddTorque(Vector3.up * (target.eulerAngles.y - oldAngle) * 10);
+        oldAngle = target.eulerAngles.y;
+    }
+}
