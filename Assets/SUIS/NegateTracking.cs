e7d05970a35c839766a742da0eaba29a0b947060
technobaboo <technobaboo@gmail.com>
2020-01-18 21:26:26 -0500

    Aligned the thingy

---
+++ Assets/SUIS/NegateTracking.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+
+public class NegateTracking : MonoBehaviour {
+    void Update() {
+        transform.localPosition = -InputTracking.GetLocalPosition(XRNode.CenterEye);
+        transform.localRotation = Quaternion.Inverse(InputTracking.GetLocalRotation(XRNode.CenterEye));
+    }
+}
