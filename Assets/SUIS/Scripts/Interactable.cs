d3dd1c14e8930ae5f42d17290f543612903946bd
Val <valmack@protonmail.com>
2020-01-18 22:58:09 -0500

    Add flick and scale implementations

--- Assets/SUIS/Scripts/Interactable.cs
+++ Assets/SUIS/Scripts/Interactable.cs
@@ -11,6 +11,9 @@ namespace SUIS {
         public InputType inputType;
         // public FallbackType fallback;
 
+        private Transform mainCamera;
+        private Transform initialHeadVector;
+
         private TextMesh debugText;
 
         var FlickMap = new Dictionary<InputType, Action>() {
@@ -35,6 +38,7 @@ namespace SUIS {
         };
 
         void Start() {
+            Vector3 initialHeadVector = mainCamera.forward;
         }
         void Update() {
             if (actionType == Flick) {
@@ -52,13 +56,26 @@ namespace SUIS {
             //
         }
         static void FlickByHand() {
-            //
+            
         }
         static void FlickByNonSpatial() {
             //
         }
         static void FlickByPointer() {
-            //
+            Flick(mainCamera);
+        }
+
+        static void Flick(Transform inputTransform) {
+            RigidBody rigidGameObject = gameObject.AddComponent<RigidBody>();
+            rigidGameObject.useGravity = false;
+
+            float initialAngle = 0;
+            var currentTorque = Vector3.up * (inputTransform.eulerAngles.y - initialAngle) * 10;
+
+            debugText.text = string.Format("DEBUG: Flick Torque = {0}", currentTorque);
+
+            rigidbody.AddTorque(currentTorque);
+            initialAngle = mainCamera.eulerAngles.y;
         }
 
         static void ScaleByController() {
@@ -71,7 +88,21 @@ namespace SUIS {
             //
         }
         static void ScaleByPointer() {
-            //
+            Scale();
+        }
+
+        static void Scale() {
+            Vector3 currentHeadVector = mainCamera.forward;
+            var cross = Vector3.Cross(initialHeadVector, currentHeadVector);
+            var currentAngle = Vector3.Angle(initialHeadVector, currentHeadVector);
+            float scale = currentAngle / 30;
+
+            if (cross.y < -0.5) {
+                dataText.text = string.Format("DEBUG: Mostly Left| Angle = {0}", currentAngle);
+            } else if (cross.y > 0.5) {
+                dataText.text = string.Format("DEBUG: Mostly Right| Angle = {0}", currentAngle);
+            }
+            transform.localScale = new Vector3(scale, scale, scale);
         }
 
         static void TranslateByController() {
